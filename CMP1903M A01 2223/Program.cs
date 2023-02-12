60a687c843861a1acacc5e8bf0e487aa6126cca0
Jack Honour <JaxkDev@gmail.com>
2023-02-12 21:41:47 +0000

    Testing class added

--- CMP1903M A01 2223/Program.cs
+++ CMP1903M A01 2223/Program.cs
@@ -6,10 +6,7 @@ namespace CMP1903M_A01_2223
     {
         static void Main(string[] args)
         {
-            new Pack();
-            for(int i = 0; i < 5; i++) Pack.shuffleCardPack(2);
-            
-            Pack.dealCard(52).ForEach(card => Console.WriteLine(card.ToString()));
+            Testing.run();
         }
     }
 }
